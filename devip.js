65d8688e55b0c031bd9c310d32f0984820d8c388
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-05 16:27:55 +0200

    Update devip.js

--- devip.js
+++ devip.js
@@ -1,18 +1,13 @@
 function devip(ws) {
-
-	if (ws._socket.remoteAddress == '::1' || ws._socket.remoteAddress == '::ffff:127.0.0.1') {
-		{
-			ws.isdeveloper = true
-			//PLAYER IS DEVLOPER
-
-		}
-	}
-
-
-
-
+  const myIP = '41.42.166.39'; // Replace this with the IP you found on Google
+  
+  if (ws._socket.remoteAddress == '::1' || 
+      ws._socket.remoteAddress == '::ffff:127.0.0.1' || 
+      ws._socket.remoteAddress.includes(myIP)) { 
+    
+    ws.isdeveloper = true; // GRANT DEVELOPER STATUS
+  }
 }
 
-devip.prototype = {
-};
+devip.prototype = {};
 module.exports = devip;
