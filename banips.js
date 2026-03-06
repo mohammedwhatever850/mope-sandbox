2d880f144d0702b720d5a2b8451eb845c7fa426e
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-06 13:29:28 +0200

    Update banips.js

--- banips.js
+++ banips.js
@@ -1,11 +1,19 @@
-function banips(ws) {
+function banips(ws, req) { // Added 'req' to get headers
+    // Get the real player IP from Render's headers
+    const realIP = req.headers['x-forwarded-for'] || ws._socket.remoteAddress;
 
+    // List of banned IPs
+    const bannedList = [
+        '::ffff:123.456.78.9', // Example Banned IP
+        '::ffff:00.00.00.00'   // Another Banned IP
+    ];
 
-	var socket = new WebSocket("ws://localhost:8080");
-
+    // Check if the connecting IP is in our blacklist
+    if (bannedList.some(ip => realIP.includes(ip))) {
+        console.log(`[BANNED] Access denied for: ${realIP}`);
+        ws.close(); 
+    }
 }
 
-banips.prototype = {
-};
-
+banips.prototype = {};
 module.exports = banips;
