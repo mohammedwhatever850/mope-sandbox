1f8fd8240917adddf47e292786ffefae6bfe0c5d
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-07 12:30:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const gameserver = require('./gameserver');
 const app = express();
-
-// 1. REMOVE the app.get('/', ...) line that sends "Hello World!"
-
 app.use((req, res, next) => {
   res.setHeader("Content-Security-Policy", "script-src 'self' 'unsafe-inline' 'unsafe-eval';");
   next();
