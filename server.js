13f86f513b92f42d8ea5d7556e30541163f9bbad
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-07 12:55:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const gameserver = require('./gameserver');
 app.use((req, res, next) => {
-  // This header tells the browser to allow 'eval' and inline scripts
-  res.setHeader(
-    "Content-Security-Policy", 
-    "default-src * 'unsafe-inline' 'unsafe-eval'; script-src * 'unsafe-inline' 'unsafe-eval'; connect-src * 'unsafe-inline'; img-src * data: blob:; frame-src *; style-src * 'unsafe-inline';"
-  );
+  res.setHeader("Content-Security-Policy", "script-src 'self' 'unsafe-inline' 'unsafe-eval';");
   next();
+});
 });
 
 // This starts your game logic
