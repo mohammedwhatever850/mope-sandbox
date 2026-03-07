576e887abf1c52e421be0f57d1c5ba5a06062b08
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-07 16:34:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,26 +5,24 @@ const gameserver = require('./gameserver');
 const app = express();
 const port = process.env.PORT || 80;
 
-// FIX 1: Set CSP Headers at the VERY TOP
+// 1. Force the CSP to allow 'eval' for your injections
 app.use((req, res, next) => {
   res.setHeader(
     "Content-Security-Policy", 
-    "default-src * 'unsafe-inline' 'unsafe-eval'; " +
-    "script-src * 'self' 'unsafe-inline' 'unsafe-eval'; " +
-    "connect-src * 'unsafe-inline'; " +
-    "img-src * data: blob:; " +
-    "style-src * 'unsafe-inline';"
+    "script-src 'self' 'unsafe-inline' 'unsafe-eval'; " +
+    "connect-src *; " + 
+    "style-src 'self' 'unsafe-inline';"
   );
   next();
 });
 
-// FIX 2: Static files must be declared BEFORE routes
+// 2. Serve all files in the main folder
 app.use(express.static(__dirname));
 
-// FIX 3: Explicitly serve sandbox.html for the main page
+// 3. Fix "Cannot GET /" by explicitly sending the file
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'sandbox.html'));
 });
 
-// FIX 4: Initialize game logic (Ensure gameserver.js doesn't have app.get('/'))
+// 4. Initialize the game logic
 new gameserver(app);
