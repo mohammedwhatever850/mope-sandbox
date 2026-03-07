611b7cd81d5327b350c2a7389dbcdcc56931775f
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-07 16:29:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,20 +5,26 @@ const gameserver = require('./gameserver');
 const app = express();
 const port = process.env.PORT || 80;
 
-// 1. CSP Header to allow eval() and injections
+// FIX 1: Set CSP Headers at the VERY TOP
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "script-src 'self' 'unsafe-inline' 'unsafe-eval';");
+  res.setHeader(
+    "Content-Security-Policy", 
+    "default-src * 'unsafe-inline' 'unsafe-eval'; " +
+    "script-src * 'self' 'unsafe-inline' 'unsafe-eval'; " +
+    "connect-src * 'unsafe-inline'; " +
+    "img-src * data: blob:; " +
+    "style-src * 'unsafe-inline';"
+  );
   next();
 });
 
-// 2. Serve static files from the main folder
+// FIX 2: Static files must be declared BEFORE routes
 app.use(express.static(__dirname));
 
-// 3. Point the main URL to your sandbox file
+// FIX 3: Explicitly serve sandbox.html for the main page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'sandbox.html'));
 });
 
-// 4. Initialize game logic
+// FIX 4: Initialize game logic (Ensure gameserver.js doesn't have app.get('/'))
 new gameserver(app);
-
