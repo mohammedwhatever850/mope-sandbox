2327e54a87431e0b6ccb10a8ddd7e4d103f96405
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-07 12:51:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ const express = require('express');
 const gameserver = require('./gameserver');
 const app = express();
 app.use((req, res, next) => {
-  res.setHeader("Content-Security-Policy", "script-src 'self' 'unsafe-inline' 'unsafe-eval';");
+ Content-Security-Policy: default-src https:; script-src https: 'unsafe-inline'; style-src https: 'unsafe-inline'
   next();
 });
 
 // This starts your game logic
 new gameserver(app);
+
 
