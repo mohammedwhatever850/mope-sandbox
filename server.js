9b820e7965c65b05a5fa3407f7c4e0d5d67c7c97
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-07 12:21:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-const game = require('./gameserver')
+const express = require('express');
+const gameserver = require('./gameserver');
+const app = express();
 
-new game(80)
+// 1. REMOVE the app.get('/', ...) line that sends "Hello World!"
+
+// 2. ADD this line to serve your HTML/JS files
+app.use(express.static(__dirname)); 
+
+// This starts your game logic
+new gameserver(app);
