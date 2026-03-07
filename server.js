9d188fb41b7c86a26e63c795214439b5933e36ba
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-07 16:15:46 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const path = require('path');
-const GameServer = require('./gameserver');
+const gameserver = require('./gameserver');
 
 const app = express();
 const port = process.env.PORT || 80;
 
-// 1. Tell Express where your game files are
-app.use(express.static(__dirname));
-
-// 2. This replaces the "Hello World" line
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// 1. CSP Header to allow eval() and injections
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "script-src 'self' 'unsafe-inline' 'unsafe-eval';");
+  next();
 });
 
-// 3. Initialize your game
-new GameServer(app);
+// 2. Serve static files from the main folder
+app.use(express.static(__dirname));
+
+// 3. Point the main URL to your sandbox file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'sandbox.html'));
+});
+
+// 4. Initialize game logic
+new gameserver(app);
+
+// 5. Start the server
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server is online on port ${port}`);
 });
