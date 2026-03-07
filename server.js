e978b8ce091b4613e49672d60a68f22cb1e2ff5e
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-07 16:07:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ app.use((req, res, next) => {
   res.setHeader("Content-Security-Policy", "script-src 'self' 'unsafe-inline' 'unsafe-eval';");
   next();
 });
-});
 
 // This starts your game logic
 new gameserver(app);
+
 
 
