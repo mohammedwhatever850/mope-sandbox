0ae14d35c53a8e966a087fecc1b847f8a4c120d9
mohammedwhatever850 <mohammedwhatever850@gmail.com>
2026-03-07 12:26:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const app = express();
 
 // 1. REMOVE the app.get('/', ...) line that sends "Hello World!"
 
-// 2. ADD this line to serve your HTML/JS files
-app.use(express.static(__dirname)); 
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "script-src 'self' 'unsafe-inline' 'unsafe-eval';");
+  next();
+});
 
 // This starts your game logic
 new gameserver(app);
+
